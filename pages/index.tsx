fbb1108c46895a5f85f877dc6a2ec24ca15a6313
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-28 21:22:03 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,23 +21,23 @@ type Props = {
 
 export const meta = {
   description:
-    "Sat Naing is a full-stack developer based in Yangon, Myanmar. He is passionate about writing codes and developing web applications to solve real-life challenges.",
-  author: "Sat Naing",
+    "Rasel Shikdar is a full-stack developer based in Tangail, Bangladesh. He is passionate about writing codes and developing web applications to solve real-life challenges.",
+  author: "Rasel Shikdar",
   type: "website",
   ogImage: `${process.env.NEXT_PUBLIC_URL}/satnaing-dev-og-new.png`,
-  siteName: "Sat Naing",
-  imageAlt: "Sat Naing portfolio website",
+  siteName: "Rasel Shikdar",
+  imageAlt: "Rasel Shikdar's Portfolio Website",
 };
 
 const Home: NextPage<Props> = ({ blogPosts }) => {
   return (
     <>
       <AppHead
-        title="Sat Naing - A Full-stack Developer"
+        title="Rasel Shikdar - A Full-stack Developer"
         url={`${process.env.NEXT_PUBLIC_URL}`}
         meta={meta}
       />
-      <Loader>SatNaing.dev</Loader>
+      <Loader>Rasel Shikdar</Loader>
       <div className="bg-bglight dark:bg-bgdark overflow-hidden">
         <div className="selection:bg-marrsgreen selection:text-bglight dark:selection:bg-carrigreen dark:selection:text-bgdark">
           <SkipToMain />
